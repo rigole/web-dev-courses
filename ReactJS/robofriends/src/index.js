9dfe9de140bacb47563ce0abbbdaa1d1008324fd
rigole <foplacide@gmail.com>
2021-07-02 12:07:37 +0100

    discovering props

--- ReactJS/robofriends/src/index.js
+++ ReactJS/robofriends/src/index.js
@@ -9,7 +9,7 @@ import 'tachyons';
 ReactDOM.render(
 
   <React.StrictMode>
-      <Hello/>
+      <Hello greeting ={"Hello React Ninja"} {}/>
   </React.StrictMode>,
   document.getElementById('root')
 
