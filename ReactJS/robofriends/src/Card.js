cda8cf9245073253742b4abd832093a23df4a277
rigole <foplacide@gmail.com>
2021-07-03 07:03:56 +0100

    creating the first Card component

---
+++ ReactJS/robofriends/src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='robots' src=''/>
+            <div>
+                <h2>Rigole</h2>
+                <p>Rigole@flashpayers.com</p>
+            </div>
+        </div>
+
+
+    );
+}
+export default Card;
