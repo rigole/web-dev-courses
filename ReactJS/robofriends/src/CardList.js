f725b09f0f05dc6709b312ace5da0c88cfacb31e
rigole <foplacide@gmail.com>
2021-07-04 23:37:28 +0100

    return all the elements of CardList.js file using map

--- ReactJS/robofriends/src/CardList.js
+++ ReactJS/robofriends/src/CardList.js
@@ -1,13 +1,14 @@
  import React from "react";
  import Card from "./Card";
- import {robots} from "./robot";
+ //import {robots} from "./robot";
 
-const CardList = () =>{
+const CardList = ({ robots }) =>{
+    const cardComponent = robots.map((user, i) => {
+        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+    })
     return (
         <div>
-            <Card id ={robots[0].id} name={robots[0].name} email = {robots[0].email}/>
-            <Card id ={robots[1].id} name={robots[1].name} email = {robots[1].email}/>
-            <Card id ={robots[2].id} name={robots[2].name} email = {robots[2].email}/>
+            {cardComponent}
         </div>
     );
 }
