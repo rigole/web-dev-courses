cc90a24a70204b408ac89a48a68d8d1cef8af0eb
rigole <foplacide@gmail.com>
2021-07-04 23:29:33 +0100

    Creating the cardList file to render the card list in index.js

--- ReactJS/robofriends/src/index.js
+++ ReactJS/robofriends/src/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
-import Card from './Card';
 import 'tachyons';
 import {robots} from "./robot";
 
 ReactDOM.render(
 
   <React.StrictMode>
-      <div>
-          <Card id ={robots[0].id} name={robots[0].name} email = {robots[0].email}/>
-          <Card id ={robots[1].id} name={robots[1].name} email = {robots[1].email}/>
-          <Card id ={robots[2].id} name={robots[2].name} email = {robots[2].email}/>
-      </div>
+          <CardList robots = {robots} />
   </React.StrictMode>,
   document.getElementById('root')
 
