7ff5d54e016a11dbc63b4511179a4febb84b67d9
rigole <foplacide@gmail.com>
2021-07-04 23:45:37 +0100

    adding unique key to each element

--- ReactJS/robofriends/src/CardList.js
+++ ReactJS/robofriends/src/CardList.js
@@ -4,7 +4,12 @@
 
 const CardList = ({ robots }) =>{
     const cardComponent = robots.map((user, i) => {
-        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+        return <Card
+            key ={i}
+            id={robots[i].id}
+            name={robots[i].name}
+            email={robots[i].email}
+        />
     })
     return (
         <div>
