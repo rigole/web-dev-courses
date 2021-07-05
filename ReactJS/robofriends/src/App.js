4c6a3ce0efc37654cffe69aeb4d903e59a59e975
rigole <foplacide@gmail.com>
2021-07-05 10:00:44 +0100

    creating state in our app

--- ReactJS/robofriends/src/App.js
+++ ReactJS/robofriends/src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import {robots} from "./robot";
 
 
-const App = () => {
-    return (
-        <div>
-            <h1>RoboFriends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
-        </div>
-    );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    render(){
+        return (
+            <div className={'tc'}>
+                <h1>RoboFriends </h1>
+                <SearchBox/>
+                <CardList robots={robots}/>
+            </div>
+        );
+    }
+
 }
 export default App;
