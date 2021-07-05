a045730a171e74aa2a3212d45db3f6e23d99d07b
rigole <foplacide@gmail.com>
2021-07-05 10:51:40 +0100

    filtering the value according to search input

--- ReactJS/robofriends/src/App.js
+++ ReactJS/robofriends/src/App.js
@@ -13,8 +13,13 @@ class App extends Component {
         }
     }
 
-    onSearchChange(event){
-        console.log(event.target.value);
+    onSearchChange = (event) => {
+        //console.log(event.target.value);
+        this.setState({searchfield: event.target.value})
+        const filteredRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes((this.state.searchfield.toLowerCase()))
+        })
+        console.log(filteredRobots);
     }
     render(){
         return (
