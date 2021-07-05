a65e20991c84b0ffb77d59fda0f697d23812c872
rigole <foplacide@gmail.com>
2021-07-05 18:17:37 +0100

    applying filter to card list in browser

--- ReactJS/robofriends/src/App.js
+++ ReactJS/robofriends/src/App.js
@@ -16,17 +16,17 @@ class App extends Component {
     onSearchChange = (event) => {
         //console.log(event.target.value);
         this.setState({searchfield: event.target.value})
+
+    }
+    render(){
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes((this.state.searchfield.toLowerCase()))
         })
-        console.log(filteredRobots);
-    }
-    render(){
         return (
             <div className={'tc'}>
                 <h1>RoboFriends </h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={this.state.robots}/>
+                <CardList robots={filteredRobots}/>
             </div>
         );
     }
